8d881c4f437048bef60480e2c834d8ab791453bb
Matthew Jackson <mnj98@vt.edu>
2021-11-23 11:51:56 -0500

    loops and sends data forever

--- rust_client_server/client.rs
+++ rust_client_server/client.rs
@@ -2,13 +2,16 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 
-
-
 fn main() -> std::io::Result<()> {
+    const BUFFER_SIZE: usize = 100000;
+
     let mut stream = TcpStream::connect("127.0.0.1:9000")?;
 
 
-    let mut buffer: [u8;10] = [4;10];
-    stream.write(&mut buffer)?;
+    let mut buffer: [u8 ; BUFFER_SIZE] = [4 ; BUFFER_SIZE];
+    //println!("{:?}", &buffer);
+    loop{
+        stream.write(&mut buffer)?;
+    }
     Ok(())
 } // the stream is closed here
--- rust_client_server/server.rs
+++ rust_client_server/server.rs
@@ -2,18 +2,39 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 
-fn handle_client(mut stream: TcpStream) {
-    let mut buffer: [u8;10] = [0;10];
-    stream.read(&mut buffer);
-    println!("{:?}", &buffer);
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()>{
+    const BUFFER_SIZE: usize = 100000;
+
+    let mut buffer: [u8 ; BUFFER_SIZE] = [0 ; BUFFER_SIZE];
+    loop{
+        match stream.read_exact(&mut buffer){
+            Ok(()) => {
+                //if size == 0 {return}
+                //println!("{:?}", &buffer);
+                /*for (index, num) in buffer.iter().enumerate() {
+                    if *num != 4 as u8{
+                        println!("bad{:?}", index);
+                        break;
+                    }
+                }*/
+                
+            },
+            Err(e) => {
+                println!("{:?}",e);
+                return Ok(());
+            }
+        }
+    }
 }
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:9000")?;
 
     // accept connections and process them serially
-    for stream in listener.incoming() {
-        handle_client(stream?);
-    }
-    Ok(())
+    Ok(for stream in listener.incoming() {
+        match handle_client(stream?){
+            Ok(()) => {return Ok(());},
+            Err(e) => {return Err(e);}
+        }
+    })
 }
