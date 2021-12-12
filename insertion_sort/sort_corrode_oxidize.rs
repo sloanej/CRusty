9f67bcdd0765301f026d47db4db176376b6cf59c
sloanej <46733794+sloanej@users.noreply.github.com>
2021-12-12 18:12:19 -0500

    adjusted curly bracket alignment

--- insertion_sort/sort_corrode_oxidize.rs
+++ insertion_sort/sort_corrode_oxidize.rs
@@ -6,11 +6,10 @@ let mut i : i32 = 1i32;
 	let tmp : i32 = *p.offset(i as (isize));
         let mut j : i32 = i;
         while j > 0i32 && (*p.offset((j - 1i32) as (isize)) > tmp) {
-	*p.offset(j as (isize)) = *p.offset((j - 1i32) as (isize));
-            j = j - 1; 
-}
+	     *p.offset(j as (isize)) = *p.offset((j - 1i32) as (isize));
+             j = j - 1; 
+	}
         *p.offset(j as (isize)) = tmp;
         i = i + 1; 
-}
-
+    }
 }
